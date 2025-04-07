43dcff7d2a74fb791c830ac42f526667a2e74588
김도현 <127471879+okiidokim@users.noreply.github.com>
2025-04-07 09:36:43 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    int fib[n+1];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i = 2; i < n+1; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    cout << fib[n];
+}

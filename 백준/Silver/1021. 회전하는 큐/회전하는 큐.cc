7f8061ea6ba04c3be5309bb7d827fdb5fe06a20e
김도현 <127471879+okiidokim@users.noreply.github.com>
2025-08-05 13:44:54 +0900

    [Silver III] Title: 회전하는 큐, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
@@ -1,45 +1,34 @@
 #include<iostream>
+#include<algorithm>
 #include<deque>
 using namespace std;
 
 int main() {
-    int n, pick;
-    cin >> n >> pick;
-    
+    int n, m;
+    cin >> n >> m;
     deque<int> dq;
-    for (int i = 0; i < n; i++) {
-        dq.push_back(i+1);
-    }
+    for (int i = 1; i <=n; i++) dq.push_back(i);
     int cnt = 0;
-    for (int i = 0; i < pick; i++){
-        int pickNum;
-        cin >> pickNum;
-        
-        deque<int> dqB = dq;
-        int tmpCnt1 = 0, tmpCnt2 = 0;
-        
-        while (dq.front() != pickNum) {
-            int tmp = dq.front();
-            dq.pop_front();
-            dq.push_back(tmp);
-            tmpCnt1++;
-        }
-        
-        while (dqB.front() != pickNum) {
-            int tmp = dqB.back();
-            dqB.pop_back();
-            dqB.push_front(tmp);
-            tmpCnt2++;
-        }
-        
-        if (tmpCnt1 < tmpCnt2) {
-            dq.pop_front();
-            cnt += tmpCnt1;
+    for (int i = 0; i < m; i++) {
+        int num;
+        cin >> num;
+        auto it = find(dq.begin(), dq.end(), num);
+        if (it - dq.begin() <= dq.size()/2) {
+            while(dq.front() != num) {
+                int tmp = dq.front();
+                dq.pop_front();
+                dq.push_back(tmp);
+                cnt++;
+            }
         } else {
-            dqB.pop_front();
-            cnt += tmpCnt2;
-            dq = dqB;
+            while(dq.front() != num) {
+                int tmp = dq.back();
+                dq.pop_back();
+                dq.push_front(tmp);
+                cnt++;
+            }
         }
+        dq.pop_front(); //얘가 루프 이후에 수행돼야 제대로 빠질 수 있음
     }
     cout << cnt;
 }

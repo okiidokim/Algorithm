ae2140a4cc7ca7fb0ef28d9900d46bbc31fc1bf1
김도현 <127471879+okiidokim@users.noreply.github.com>
2025-08-05 13:07:53 +0900

    [Silver II] Title: 나무 자르기, Time: 152 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -1,31 +1,35 @@
 #include<iostream>
-#include<algorithm>
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0); //1. 시간 초과 -> 코드 추가
-    cin.tie(NULL);
-    int m, h;
-    cin >> m >> h;
-    int tree[m];
-    for (int i = 0; i < m; i++) 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n, m;
+    cin >> n >> m;
+    int tree[n];
+    int right = 0;
+    for (int i = 0; i < n; i++) {
         cin >> tree[i];
-    sort(tree, tree+m);
-    int low = 0, high = tree[m-1], mid, len;
-    //첫 채점 전 예제 5을 통해 low 값을 tree[0]이 아닌 0으로 수정
-    while (low <= high && low >= 0) {
-        long long sum = 0; //2. 틀렸습니다 -> 자료형 수정
-        mid = (low + high) / 2;
-        for (int i = 0; i < m; i++) {
-            if (mid < tree[i]) {
-                sum += tree[i] - mid;
-            }
-        }
-        if (sum >= h) { //3. 틀렸습니다 -> 왜지.. 근데 여기 문제는 맞음
-            low = mid + 1; //4. 시간초과 -> 아니 미띤넘ㅇ 둘다 -1 함 ㄷㄷ
-            len = mid;
-        }
-        else high = mid - 1;
+        right = max(tree[i], right);
     }
-    cout << len << "\n";
+    int left = 1;
+    int mid = (right + left) /2;
+    int ans = 0;
+    while(left <= right) {
+        long long sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += tree[i] - mid < 0 ? 0 : tree[i] - mid; //0보다 작을 때는 더하면 안 되고 그냥 더하면 안 돼서 삼항 연산자 추가
+        }
+        if (sum < m) {
+            right = mid-1;
+        } else {
+            ans = mid;
+            left = mid + 1;
+        }
+        mid = (right + left) / 2;
+    }
+    cout << ans;
 }
+
+//1차 시도 2% 틀림
+//2차 시도 2% 틀림

6f013734996a52901d451e38db5505965d6d43e1
김도현 <127471879+okiidokim@users.noreply.github.com>
2025-08-19 17:55:49 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.81 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,11 @@
+function solution(arr1, arr2) {
+    var answer = [];
+    for (let i = 0; i < arr1.length; i++) {
+        let col = [];
+        for (let j = 0; j < arr1[0].length; j++) {
+            col.push(arr1[i][j]+arr2[i][j]);
+        }
+        answer.push(col);
+    }
+    return answer;
+}

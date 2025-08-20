5efda788c09c4a130dfe6746fe421773320b25a7
김도현 <127471879+okiidokim@users.noreply.github.com>
2025-08-20 11:41:33 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.92 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+    var answer = [];
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i+1; j < numbers.length; j++) {
+            const num = numbers[i] + numbers[j];
+            if (answer.includes(num)) continue;
+            answer.push(num);
+        }
+    }
+    answer.sort((a,b) => a-b);
+    return answer;
+}

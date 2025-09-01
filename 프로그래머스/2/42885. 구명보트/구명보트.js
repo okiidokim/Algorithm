53b903d2768a72e92c35858b5cb50c375a288e76
김도현 <127471879+okiidokim@users.noreply.github.com>
2025-09-01 20:26:01 +0900

    [level 2] Title: 구명보트, Time: 32.67 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,13 @@
+function solution(people, limit) {
+    var answer = 0;
+    people.sort((a,b) => a- b);
+    let curr = 0;
+    let left = 0;
+    let right = people.length -1;
+    while(left <= right) {
+        if (people[left] + people[right] <= limit) left++;
+        right--;
+        answer++;
+    }
+    return answer;
+}

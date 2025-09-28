4447e0d505a8eaa150df319e0387d07399bbc68b
김도현 <127471879+okiidokim@users.noreply.github.com>
2025-09-28 20:15:35 +0900

    [level 2] Title: 2 x n 타일링, Time: 0.23 ms, Memory: 3.81 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int MOD = 1000000007;
+    int f[n+1];
+    f[0] = 1; f[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        f[i] = f[i-1] + f[i-2];
+        f[i] %= MOD;
+    }
+    return f[n];
+}

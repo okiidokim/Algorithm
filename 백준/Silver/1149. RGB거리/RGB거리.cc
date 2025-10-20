3bdf2c3f5dce27d57add1e2162b73b217577d058
김도현 <127471879+okiidokim@users.noreply.github.com>
2025-10-20 23:21:26 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int colors[n][3];
+    for (int i = 0; i < n; i++) {
+        cin >> colors[i][0] >> colors[i][1] >> colors[i][2];
+    }
+    vector<vector<int>> dp(n, vector<int>(3,0));
+    dp[0][0] = colors[0][0];
+    dp[0][1] = colors[0][1];
+    dp[0][2] = colors[0][2];
+
+    for (int i = 1; i < n; i++) {
+        dp[i][0] = colors[i][0] + min(dp[i-1][1], dp[i-1][2]);
+        dp[i][1] = colors[i][1] + min(dp[i-1][0], dp[i-1][2]);
+        dp[i][2] = colors[i][2] + min(dp[i-1][0], dp[i-1][1]);
+    }
+
+    cout << min({dp[n-1][0], dp[n-1][1], dp[n-1][2]});
+}

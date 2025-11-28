0ed274a2fc3b011afaac06c8d528fd13078fa855
김도현 <127471879+okiidokim@users.noreply.github.com>
2025-11-28 11:15:05 +0900

    [Gold IV] Title: 사전 순 최대 공통 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.cc
+++ 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.cc
@@ -1,64 +1,51 @@
-// 13:32 ~ 
+//10:50 ~ 11:14 (25분)
 #include<iostream>
 #include<vector>
 #include<algorithm>
 using namespace std;
 
-int main() {
-    int maxNum = 0;
+int maxNum;
 
+int findTarget(vector<int> arr, int idx) {
+    for (int i = idx; i < arr.size(); i++) {
+        if (arr[i] == maxNum) {
+            idx = i;
+            return idx;
+        }
+    }
+    return -1;
+}
+int main() {
     int n;
     cin >> n;
-    int arr[n];
+    vector<int> arr(n);
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
         maxNum = max(arr[i], maxNum);
     }
-
     int m;
     cin >> m;
-    int brr[m];
+    vector<int> brr(m);
     for (int i = 0; i < m; i++) {
         cin >> brr[i];
         maxNum = max(brr[i], maxNum);
     }
 
-    int aIdx = 0, bIdx = 0;
     vector<int> answer;
-    
-    while(1) {
-        bool found = false;
-        for (int i = maxNum; i > 0; i--) {
-            int idxFromA = -1;
-            for (int j = aIdx; j < n; j++) {
-                if (arr[j] == i) {
-                    idxFromA = j;
-                    break;
-                }
-            }
-            if (idxFromA == -1) continue;
-
-            int idxFromB = -1;
-            for (int j = bIdx; j < m; j++) {
-                if (brr[j] == i) {
-                    idxFromB = j;
-                    break;
-                }
-            }
-            if (idxFromB == -1) continue;
-
-            aIdx = idxFromA+1;
-            bIdx = idxFromB+1;
-            answer.push_back(arr[idxFromA]);
-            
-            found = true;
-            break;
+    int aIdx = 0, bIdx = 0;
+    while (1) {
+        if (maxNum == 0) break;
+        if (aIdx == n || bIdx == m) break;
+        int tmpAIdx = findTarget(arr, aIdx);
+        int tmpBIdx = findTarget(brr, bIdx);
+        if (tmpAIdx == -1 || tmpBIdx == -1) {
+            maxNum--;
+            continue;
         }
-        if (!found) break;
+        answer.push_back(maxNum);
+        aIdx = tmpAIdx + 1;
+        bIdx = tmpBIdx + 1;
     }
-
-    cout << answer.size() << "\n";
-    for (int num : answer) {
-        cout << num << " ";
-    }
+    cout << answer.size() << '\n';
+    for (int n : answer) cout << n << " ";
 }

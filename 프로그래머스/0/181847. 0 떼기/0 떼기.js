ce6287c32cd92d2fdd5367228b4f5d1eb9c3932c
김도현 <127471879+okiidokim@users.noreply.github.com>
2025-12-26 16:05:02 +0900

    [level 0] Title: 0 떼기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -0,0 +1,9 @@
+function solution(n_str) {
+    var answer = '';
+    for (let i = 0; i < n_str.length; i++) {
+        if (n_str[i] === '0') continue;
+        answer = n_str.slice(i);
+        break;
+    }
+    return answer;
+}
